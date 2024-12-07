8f7865c10e2b0d020d92f1a61cdde8d7f0973692
ivy <xieyang@dodora.cn>
2024-12-07 16:17:44 +0800

    feat: update fellou blog title

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -12,8 +12,9 @@ interface SiteConfig {
 export const siteConfig: SiteConfig = {
 	site: 'https://fellou.ai', // Write here your website url
 	author: 'Fellou', // Site author
-	title: 'Fellou Blog', // Site title.
-	description: 'Everything you need to know about AI browser, agent programming framework, a totaly new Agent OS. Company updates &amp; Technology Trends.', // Description to display in the meta tags
+	title: 'Fellou Blog - The next generation AI Browser', // Site title.
+	description:
+		'Everything you need to know about AI browser, agent programming framework, a totaly new Agent OS. Company updates &amp; Technology Trends.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
