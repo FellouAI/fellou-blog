dcf1966e183750f4d54deedc72ac24e4fc888382
codevoyager.jay <codevoyager.jay@gmail.com>
2025-01-21 15:58:42 +0800

    fix: Typo

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,5 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = [
-	'Product',
-	'Developers',
-] as const
+export const CATEGORIES = ['Product', 'Developers', 'Agent framework'] as const
